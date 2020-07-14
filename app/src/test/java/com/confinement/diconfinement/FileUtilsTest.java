fd7dcd432429ae555b663df8742ddbaa942eb300
flinou <trasheaven@hotmail.fr>
2020-07-14 13:57:09 +0200

    Fix error in tests
    Signed-off-by: flinou <trasheaven@hotmail.fr>

--- app/src/test/java/com/confinement/diconfinement/FileUtilsTest.java
+++ app/src/test/java/com/confinement/diconfinement/FileUtilsTest.java
@@ -1,29 +1,34 @@
 package com.confinement.diconfinement;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
+import static com.confinement.diconfinement.FileUtils.populateDicoWords;
 import static org.junit.Assert.assertTrue;
 
 public class FileUtilsTest {
 
     private static String eclairAccentTest = "éclair";
-    private static String eclairTest = "eclair";
     private static String reverAccentTest = "rêver";
-    private static String reverTest = "rêver";
     private static String guereAccentTest = "guère";
-    private static String guereTest = "guere";
     private static String batimentAccentTest = "bâtiment";
-    private static String batimentTest = "batiment";
     private static String maisAccentTest = "maïs";
-    private static String maisTest = "mais";
 
     private static String smallword = "a";
     private static String smallwordTwo = "le";
+    InputStream is = null;
+    TreeSet<String> dicoWords = null;
 
+    @Before
+    public void initResources() {
+        is = this.getClass().getClassLoader().getResourceAsStream("dico.txt");
+        dicoWords = populateDicoWords(is);
+    }
 
     @Test
     public void filetoSearch() {
@@ -41,38 +46,31 @@ public class FileUtilsTest {
     public void retrieveSuggestions() throws IOException {
         //Testing accent é
         suggestionTest(eclairAccentTest, "éclair");
-        suggestionTest(eclairTest, "éclair");
 
         //Testing accent ê
         suggestionTest(reverAccentTest, "rêver");
-        suggestionTest(reverTest, "rêver");
 
         //Testing accent è
         suggestionTest(guereAccentTest, "guère");
-        suggestionTest(guereTest, "guère");
 
         //Testing accent â
         suggestionTest(batimentAccentTest, "bâtiment");
-        suggestionTest(batimentTest, "bâtiment");
 
         //Testing accent ï
         suggestionTest(maisAccentTest, "maïs");
-        suggestionTest(maisTest, "maïs");
 
         noSuggestionTest(smallword);
         noSuggestionTest(smallwordTwo);
     }
 
-    private void noSuggestionTest(String smallword) throws IOException {
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("dico.txt");
-        ArrayList<String> suggestions = FileUtils.retrieveSuggestions(is, smallword);
+    private void noSuggestionTest(String wordToFind) throws IOException {
+        ArrayList<String> suggestions = FileUtils.retrieveSuggestions(dicoWords, wordToFind);
         assertTrue(smallword + " should not have any suggestions retrieved ", suggestions.isEmpty());
         is.close();
     }
 
     public void suggestionTest(String searchedWord, String wordToFind) throws IOException {
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream("dico.txt");
-        ArrayList<String> suggestions = FileUtils.retrieveSuggestions(is, searchedWord);
+        ArrayList<String> suggestions = FileUtils.retrieveSuggestions(dicoWords, searchedWord);
         assertTrue(wordToFind + " has not been found in suggestions with : " + searchedWord, suggestions.contains(wordToFind));
         is.close();
     }
