7c8d507c1f040b42923840fc3107191b314ca7c7
Antoine Flinois <antoine.flinois@hotmail.com>
2021-11-27 19:35:44 +0100

    Remove useless tests
    Signed-off-by: Antoine Flinois <antoine.flinois@hotmail.com>

--- app/src/androidTest/java/com/confinement/diconfinement/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/confinement/diconfinement/ExampleInstrumentedTest.java
@@ -6,15 +6,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -31,25 +23,5 @@ public class ExampleInstrumentedTest {
         assertEquals("com.confinement.diconfinement", appContext.getPackageName());
     }
 
-    @Test
-    public void handleIntentTest() throws IOException {
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        FileUtils.initFirstWordDicoHashMap(appContext);
 
-        int dictionaryId = appContext.getResources().getIdentifier("dico","raw", "com.confinement.diconfinement");
-        InputStream is = appContext.getResources().openRawResource(dictionaryId);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String missingWords = "";
-        while(reader.ready()) {
-            ArrayList<String> definitionList = new ArrayList<>();
-            String dicoWord = reader.readLine();
-            try {
-                assertTrue("Le mot" + dicoWord + "n'a pas de définition", DefinitionsFinder.getDefinitions(appContext.getResources(), dicoWord, definitionList));
-            } catch (AssertionError err) {
-                missingWords += dicoWord + "\n";
-            }
-        }
-        System.out.println(missingWords);
-        assertTrue(missingWords.length() > 0);
-    }
 }
