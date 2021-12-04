8d9eec26f4351efd0e6662943de66054386443e8
Antoine Flinois <antoine.flinois@hotmail.com>
2021-12-04 15:43:13 +0100

    Improve notification content
    Signed-off-by: Antoine Flinois <antoine.flinois@hotmail.com>

--- app/src/main/java/com/confinement/diconfinement/AlarmReceiver.java
+++ app/src/main/java/com/confinement/diconfinement/AlarmReceiver.java
@@ -8,7 +8,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
@@ -16,39 +20,49 @@ import static android.content.Context.NOTIFICATION_SERVICE;
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (FileUtils.needsNotification(context, FileUtils.getDay())) {
-            Intent intentMainAct = new Intent(context, MainActivity.class);
-            intentMainAct.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentMainAct, 0);
-            NotificationManager notificationManager = (NotificationManager)
-                    context.getSystemService(NOTIFICATION_SERVICE);
-            Notification notif;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                setChannelForNotif(notificationManager);
-                notif = createNotif(context, pendingIntent);
-            } else {
-                notif = createNotifOldAndroid(context, pendingIntent);
-            }
-            notificationManager.notify(0, notif);
-            SharedPrefUtils.updateLastNotificationDate(context, FileUtils.getDay());
+        Intent intentMainAct = new Intent(context, MainActivity.class);
+        intentMainAct.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //Put Extra String to handle application start from notification (must lands to word of the day tab at startup)
+        intentMainAct.putExtra(Globals.notification, true);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intentMainAct, 0);
+        NotificationManager notifManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        Notification notif;
+        String wordOfTheDay = WordOfTheDayUtils.retrieveCurrentWordOfTheDay(context.getApplicationContext());
+        Spannable notifContent = generateNotifContent(context, wordOfTheDay);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setChannelForNotif(notifManager);
+            notif = createNotif(context, pendingIntent, notifContent);
+        } else {
+            notif = createNotifOldAndroid(context, pendingIntent, notifContent);
         }
+        notifManager.notify(0, notif);
     }
 
-    private Notification createNotifOldAndroid(Context context, PendingIntent pendingIntent) {
+    @NonNull
+    private Spannable generateNotifContent(Context context, String notifContent) {
+        String wordOdTheDayFormat = context.getString(R.string.wordOfTheDay_notif);
+        int wordDayStartPos = wordOdTheDayFormat.indexOf("%1$s");
+        String lineFormatted = context.getString(R.string.wordOfTheDay_notif, notifContent);
+        Spannable wordOfTheDayNotif = new SpannableString(lineFormatted);
+        wordOfTheDayNotif.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), wordDayStartPos, wordDayStartPos + notifContent.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return wordOfTheDayNotif;
+    }
+
+    private Notification createNotifOldAndroid(Context context, PendingIntent pendingIntent, Spannable notifContent) {
         return new Notification.Builder(context)
                 .setContentTitle(Globals.notification_title)
-                .setSmallIcon(R.mipmap.ic_lanceur)
-                .setContentText(Globals.notification_content)
+                .setSmallIcon(R.drawable.ic_dico_notif)
+                .setContentText(notifContent)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true).build();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private Notification createNotif(Context context, PendingIntent pendingIntent) {
+    private Notification createNotif(Context context, PendingIntent pendingIntent, Spannable wordOfTheDay) {
         return new Notification.Builder(context, Globals.channel_id)
                 .setContentTitle(Globals.notification_title)
-                .setSmallIcon(R.mipmap.ic_lanceur)
-                .setContentText(Globals.notification_content)
+                .setSmallIcon(R.drawable.ic_dico_notif)
+                .setContentText(wordOfTheDay)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true).build();
     }
